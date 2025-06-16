3c8631ebe2930983a099647995f2884ee7df67d5
Fidel Morales <fidel.morales.briones@gmail.com>
2025-06-16 05:54:51 -0600

    changed title and favicon

--- front/src/components/BasicTable.tsx
+++ front/src/components/BasicTable.tsx
@@ -6,7 +6,6 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import Button from '@mui/material/Button'
 import { useDispatch, useSelector } from 'react-redux';
 import type { AppDispatch, RootState } from '../state/store';
 import { useEffect } from 'react';
